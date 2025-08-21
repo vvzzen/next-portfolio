bc69ad9cd25c35c91aa3b03618ac3b860078ed14
vvzzen <alvin.dhea@gmail.com>
2025-08-21 07:04:32 +0700

    update cv href

--- components/ui/ResumeButton.tsx
+++ components/ui/ResumeButton.tsx
@@ -3,9 +3,9 @@ import React from 'react'
 const ResumeButton = () => {
   return (
 <button className="bg-slate-800 no-underline group cursor-pointer relative shadow-2xl shadow-zinc-900 rounded-full p-1 text-lg font-bold leading-7 text-white inline-block " >
-    <a target="_blank" rel="noopener noreferrer" href='next-portfolio/Alvin CV-20 Aug 25-Technical.pdf'>
+    <a target="_blank" rel="noopener noreferrer" href='/next-portfolio/Alvin CV-20 Aug 25-Technical.pdf'>
   <span className="absolute inset-0 overflow-hidden rounded-full">
-    <span className="absolute inset-0 rounded-full bg-[image:radial-gradient(75%_100%_at_50%_0%,rgba(56,189,248,0.6)_0%,rgba(56,189,248,0)_75%)] opacity-0 transition-opacity duration-500 group-hover:opacity-100" />
+    <span className="absolute inset-0 rounded-full bg-[image:radial-gradient(75%_100%_at_50%_0%,rgba(56,/189,248,0.6)_0%,rgba(56,189,248,0)_75%)] opacity-0 transition-opacity duration-500 group-hover:opacity-100" />
   </span>
   <div className="relative flex space-x-3 items-center z-10 rounded-full bg-zinc-950 py-2 px-8 ring-1 ring-white/10 ">
     <span className="text-xl font-extrabold tracking-wide">
